39d86282f02aef40c4fde492f2dcba5a693ecd0e
Njoyp <nikita_pieters@hotmail.com>
2023-01-12 11:58:27 +0100

    new files first module

---
+++ comments/main.py
@@ -0,0 +1,12 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+print ("Hello") # greetings
+print ("Welcome to my code") # a word of welcome
+""""
+Here there is text for an example of a multi line
+comment, but i do not know if this is correct
+"""
---
+++ variables/main.py
@@ -0,0 +1,8 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+example1 = "Hello, I am a variable"
+example2 = 5
+example3 = True
