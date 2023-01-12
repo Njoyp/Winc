a35d1157e13bc0d7ec97d824b149cca0650abaf6
Njoyp <nikita_pieters@hotmail.com>
2023-01-12 15:24:53 +0100

    two new files

---
+++ String.py
@@ -0,0 +1,11 @@
+String1 = "I am a normal string"
+String2 = 'I too am a normal string'
+String3 = "I'm a special string"
+String4 = "He said: \"I'm special\""
+
+print (String1)
+print (String2)
+print (String3)
+print (String4)
+
+print ("And I too \n am special")
---
+++ Type.py
@@ -0,0 +1,12 @@
+print (type (3))
+print (type (3.14))
+print (type ('Pi'))
+
+some_number = 5
+print (type(some_number))
+another_number = 9.81
+print (type(another_number))
+some_string = 'Hello world'
+print (type(some_string))
+
+print ("Hello, 2+2=" + str(2+2))
